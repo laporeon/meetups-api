9320003fec663fd15ec16f6ec82d27c82182488a
Laíse Carvalho <34722707+laporeon@users.noreply.github.com>
2022-01-04 18:01:49 -0300

    feat(auth): protect event routes

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 
 const EventController = require("../controllers/EventController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const eventRoutes = Router();
 
 eventRoutes.get("/", EventController.list);
-eventRoutes.post("/", EventController.create);
-eventRoutes.delete("/:id", EventController.delete);
+eventRoutes.post("/", ensureAuthenticated, EventController.create);
+eventRoutes.delete("/:id", ensureAuthenticated, EventController.delete);
 
 module.exports = eventRoutes;
